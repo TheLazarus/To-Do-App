14f325e64a58a11ff2574b94c23412709bc2405e
= <=>
2020-05-14 02:41:02 -0400

    Added Main App HTML and JavaScript

---
+++ app.js
@@ -0,0 +1,27 @@
+const vm = new Vue({            //main VUe instance
+
+        el: '#todoapp',
+        data: {
+
+            title: 'Hello World',
+            newtodo: '',
+            todoList: []
+        },
+        methods: {
+            addTodo(){ // pushin gnew todos
+                console.log(this.newtodo);
+                this.todoList.push({
+                title : this.newtodo,
+                done: false
+                });
+                this.newtodo = ""
+            },
+            removeTodo(todo){
+                const index = this.todoList.indexOf(todo);
+                this.todoList.splice(index,1);
+            }
+        }
+
+
+
+})
